972afd1d824e51ab28ac9c35d8d00e382e138bf6
FUKUSHIMA Kaito <fukushima@jaist.ac.jp>
2019-05-30 15:41:38 +0900

    fix asknowintent

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def now():
     with urllib.request.urlopen(req) as res:
         data = json.loads(res.read().decode('utf8'))
 
-    msg = render_template('now', date=data[0]['created'],vib=data[0]['d1'],light=data[0]['d2'])
+    msg = render_template('now', date=data[0]['created'] vib=data[0]['d1'] light=data[0]['d2'])
     return question(msg)
     
 if __name__ == '__main__':
