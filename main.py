e1087d537ab1f5058b8961ce8eba761fdf46f66e
FUKUSHIMA Kaito <fukushima@jaist.ac.jp>
2019-05-30 15:23:34 +0900

    add asknowintent

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 import logging
-from random import shuffle
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
-from sympy import sieve
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -13,18 +11,19 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @ask.launch
 def introduction():
-    welcome_msg = render_template('welcome')
-    return question(welcome_msg)
+    msg = render_template('welcome')
+    return question(msg)
 
-@ask.intent("YesIntent")
-def start():
-    start_msg = render_template('start')
-    return question(start_msg)
+@ask.intent("AMAZON.HelpIntent")
+def help():
+    msg = render_template('help')
+    return question(msg)
 
-# @ask.intent("AnswerIntent", convert={'prime': int})
-# def answer(prime):
-#     if type(prime) != int:
-#     return statement(msg)
-
+@ask.intent("AskNowdata")
+def now():
+    data = createdata()
+    msg = render_template('now', date=data[0]['created'],vib=data[0]['d1'],light=data[0]['d2'])
+    return question(msg)
+    
 if __name__ == '__main__':
     app.run(debug=True)
