57cf23e90aad45811fbd6af72cde86775ddb5ea8
FUKUSHIMA Kaito <fukushima@jaist.ac.jp>
2019-05-30 15:35:53 +0900

    add asknowintent

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+# coding: UTF-8
 
 import logging
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 
-import createdata as cd
+import urllib.request
+import json
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -23,7 +25,12 @@ def help():
 
 @ask.intent("AskNowdata")
 def now():
-    data = cd.createdata()
+
+    url = "http://ambidata.io/api/v2/channels/10905/data?readKey=7e7df40858ef249c&n=1"
+	req = urllib.request.Request(url)
+	with urllib.request.urlopen(req) as res:
+        data = json.loads(res.read().decode('utf8'))
+
     msg = render_template('now', date=data[0]['created'],vib=data[0]['d1'],light=data[0]['d2'])
     return question(msg)
     
